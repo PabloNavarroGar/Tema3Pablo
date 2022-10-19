8519701c3a304e731acc963c6f03a6a0cc2348e6
Pablodaw2022 <pnavgarb@g.educaand.es>
2022-10-19 20:06:57 +0200

    COmmit

--- src/main/java/ejerciciosPracticosVico/FabricaDulcesNavideños.java
+++ src/main/java/ejerciciosPracticosVico/FabricaDulcesNavideños.java
@@ -20,18 +20,24 @@ public class FabricaDulcesNavideños {
 
         //Variables constantes
         //MantecadosLimon y Turronchocolate
-        final double COSTEMANOOBRAM1, COSTEMANOOBRAT1 = 0.15;
+        //Control+R para cmabiar el nombre de una variable y se cambiene en todas
+        final double COSTEMANOOBRAM1T1 = 0.15;
+
         //Polvorones,turron clasico y mazapanes
-        final double COSTEMANOOBRAM2, COSTEMANOOBRAT2, COSTEMANOOBRAP1 = 0.22;
+        final double COSTEMANOOBRAM2T2P1 = 0.22;
+
         //Variables
         double costeMateriaPrima;//Introducir en el panel entre 0,1 y 1€
-        double costeProduccion;
+        double costeProduccionUnidad;
+        double precioVenta;
+        double beneficio;
+        double objetivo;
+
         //FORMULA PRECIO VENTA
         //Artículos M1, M2 y P1. 
-        //Precio de venta unitario = coste de producción + 50 % del coste de producción
+        //Precio de venta unitario = coste de producción +50 % del coste de producción
         //Resto de artículos(T1 y T2).
         //Precio de venta unitario = coste de producción + 65 % del coste de producción
-
         String menu = """
                                 "La Esteponera Turronera"
                     -----------------------------------------------------------------
@@ -51,16 +57,202 @@ public class FabricaDulcesNavideños {
             case "1":
                 //Mantecadosde limon---> M1
                 //coste producción = materia prima + mano de obra
-                JOptionPane.showMessageDialog(null, " "
-                        + "A continuacion tienes que meter el precio entre 0.1€ "
-                        + "o 1€ si no, no sera valido");
-                String costeMateriaString = JOptionPane.showInputDialog(" Introduce el peso");
-                //Pase
+                String costeString = JOptionPane.showInputDialog("Introduce el coste de la materia prima(entre 0.1 y 1)");
+
+                costeMateriaPrima = Double.parseDouble(costeString);
+
+                JOptionPane.showMessageDialog(null, "El coste de la materia es de " + costeMateriaPrima);
+
+                if (costeMateriaPrima >= 0.1 && costeMateriaPrima <= 1) {
+
+                    //Calculo coste unidad
+                    costeProduccionUnidad = COSTEMANOOBRAM1T1 + costeMateriaPrima;
+
+                    //Calculo del precio de venta
+                    precioVenta = costeProduccionUnidad + (costeProduccionUnidad * 0.5);
+
+                    //Calculo beneficio
+                    beneficio = precioVenta - costeProduccionUnidad;
+                    //salida de datos
+                    String resultado = """
+                                        Un Mantecado de Limon (M1) nos cuesta : %.2f Centimos
+                                        El precio de venta es de: %.2f Centimos
+                                        El beneficio es de: %.2f Centimos
+                                     """.formatted(costeProduccionUnidad, precioVenta, beneficio);
+                    JOptionPane.showMessageDialog(null, resultado);
+
+                    JOptionPane.showMessageDialog(null, "Se va a calcular cuantos unidades a producir para que haya un beneficio de al menos 2500€");
+                    objetivo = 2500 * 1 / beneficio;
+                    //(int para declarar el double en int y el ceil para redondear hacia arriba)
+                    JOptionPane.showMessageDialog(null, " Necesitamos fabricar " + (int) Math.ceil(objetivo) + " Mantecados ");
+                    //Pase
+                } else {
+                    JOptionPane.showMessageDialog(null, " Error, precio no permitido");
+
+                }
+
+                break;
+
+            case "2":
+
+                //Turron Chococate---> T1
+                //coste producción = materia prima + mano de obra
+                String coste2String = JOptionPane.showInputDialog("Introduce el coste de la materia prima(entre 0.1 y 1)");
+
+                costeMateriaPrima = Double.parseDouble(coste2String);
+
+                JOptionPane.showMessageDialog(null, "El coste de la materia es de " + costeMateriaPrima);
+
+                if (costeMateriaPrima >= 0.1 && costeMateriaPrima <= 1) {
+
+                    //Calculo coste unidad
+                    costeProduccionUnidad = COSTEMANOOBRAM1T1 + costeMateriaPrima;
+
+                    //Calculo del precio de venta
+                    precioVenta = costeProduccionUnidad + (costeProduccionUnidad * 0.65);
+
+                    //Calculo beneficio
+                    beneficio = precioVenta - costeProduccionUnidad;
+
+                    String resultado = """
+                                        Un Turron de chocolate (T1) nos cuesta : %.2f Centimos
+                                        El precio de venta es de: %.2f Centimos
+                                        El beneficio es de: %.2f Centimos
+                                     """.formatted(costeProduccionUnidad, precioVenta, beneficio);
+                    JOptionPane.showMessageDialog(null, resultado);
+
+                    JOptionPane.showMessageDialog(null, "Se va a calcular cuantos unidades a producir para que haya un beneficio de al menos 2500€");
+                    objetivo = 2500 * 1 / beneficio;
+                    //(int para declarar el double en int y el ceil para redondear hacia arriba)
+                    JOptionPane.showMessageDialog(null, " Necesitamos fabricar " + (int) Math.ceil(objetivo) + " Turrones ");
+                    //Pase
+                } else {
+                    JOptionPane.showMessageDialog(null, " Error, precio no permitido");
+
+                }
+
+                break;
+
+            case "3":
+
+                //Polvorones--->M2
+                //coste producción = materia prima + mano de obra
+                String coste3String = JOptionPane.showInputDialog("Introduce el coste de la materia prima(entre 0.1€ y 1€)");
+
+                costeMateriaPrima = Double.parseDouble(coste3String);
+
+                JOptionPane.showMessageDialog(null, "El coste de la materia es de " + costeMateriaPrima);
+
+                if (costeMateriaPrima >= 0.1 && costeMateriaPrima <= 1) {
+
+                    //Calculo coste unidad
+                    costeProduccionUnidad = COSTEMANOOBRAM2T2P1 + costeMateriaPrima;
+
+                    //Calculo del precio de venta
+                    precioVenta = costeProduccionUnidad + (costeProduccionUnidad * 0.5);
+
+                    //Calculo beneficio
+                    beneficio = precioVenta - costeProduccionUnidad;
+
+                    String resultado = """
+                                        Un Polvoron (M2) nos cuesta : %.2f Centimos
+                                        El precio de venta es de: %.2f Centimos
+                                        El beneficio es de: %.2f Centimos
+                                     """.formatted(costeProduccionUnidad, precioVenta, beneficio);
+                    JOptionPane.showMessageDialog(null, resultado);
+
+                    JOptionPane.showMessageDialog(null, "Se va a calcular cuantos unidades a producir para que haya un beneficio de al menos 2500€");
+                    objetivo = 2500 * 1 / beneficio;
+                    JOptionPane.showMessageDialog(null, " Necesitamos fabricar " + (int) Math.ceil(objetivo) + " Polvorones");
+                    //Pase
+                } else {
+                    JOptionPane.showMessageDialog(null, " Error, precio no permitido");
+
+                }
+
+                break;
+
+            case "4":
+
+                //Turron clasico---> T2
+                //coste producción = materia prima + mano de obra
+                String coste4String = JOptionPane.showInputDialog("Introduce el coste de la materia prima(entre 0.1€ y 1e)");
+
+                costeMateriaPrima = Double.parseDouble(coste4String);
+
+                JOptionPane.showMessageDialog(null, "El coste de la materia es de " + costeMateriaPrima);
+
+                if (costeMateriaPrima >= 0.1 && costeMateriaPrima <= 1) {
+
+                    //Calculo coste unidad
+                    costeProduccionUnidad = COSTEMANOOBRAM2T2P1 + costeMateriaPrima;
+
+                    //Calculo del precio de venta
+                    precioVenta = costeProduccionUnidad + (costeProduccionUnidad * 0.65);
+
+                    //Calculo beneficio
+                    beneficio = precioVenta - costeProduccionUnidad;
+                    //salida de datos
+                    String resultado = """
+                                        Un Turron de chocolate (T1) nos cuesta : %.2f Centimos
+                                        El precio de venta es de: %.2f Centimos
+                                        El beneficio es de: %.2f Centimos
+                                     """.formatted(costeProduccionUnidad, precioVenta, beneficio);
+                    JOptionPane.showMessageDialog(null, resultado);
+
+                    JOptionPane.showMessageDialog(null, "Se va a calcular cuantos unidades a producir para que haya un beneficio de al menos 2500€");
+                    objetivo = 2500 * 1 / beneficio;
+                    //(int para declarar el double en int y el ceil para redondear hacia arriba)
+                    JOptionPane.showMessageDialog(null, " Necesitamos fabricar " + (int) Math.ceil(objetivo) + " Turrones clasicos ");
+                    //Pase
+                } else {
+                    JOptionPane.showMessageDialog(null, " Error, precio no permitido");
+
+                }
+
+                break;
+
+            case "5":
+                //Mazapanes---> P1
+                //coste producción = materia prima + mano de obra
+                String coste5String = JOptionPane.showInputDialog("Introduce el coste de la materia prima(entre 0.1 y 1)");
+
+                costeMateriaPrima = Double.parseDouble(coste5String);
+
+                JOptionPane.showMessageDialog(null, "El coste de la materia es de " + costeMateriaPrima);
+
+                if (costeMateriaPrima >= 0.1 && costeMateriaPrima <= 1) {
+
+                    //Calculo coste unidad
+                    costeProduccionUnidad = COSTEMANOOBRAM2T2P1 + costeMateriaPrima;
+
+                    //Calculo del precio de venta
+                    precioVenta = costeProduccionUnidad + (costeProduccionUnidad * 0.5);
+
+                    //Calculo beneficio
+                    beneficio = precioVenta - costeProduccionUnidad;
+
+                    String resultado = """
+                                        Un Polvoron (M2) nos cuesta : %.2f Centimos
+                                        El precio de venta es de: %.2f Centimos
+                                        El beneficio es de: %.2f Centimos
+                                     """.formatted(costeProduccionUnidad, precioVenta, beneficio);
+                    JOptionPane.showMessageDialog(null, resultado);
+
+                    JOptionPane.showMessageDialog(null, "Se va a calcular cuantos unidades a producir para que haya un beneficio de al menos 2500€");
+                    objetivo = 2500 * 1 / beneficio;
+                    //Int para declarar la variable se muestre en int y ceil para redondear hacia arriba
+                    JOptionPane.showMessageDialog(null, " Necesitamos fabricar " + (int) Math.ceil(objetivo) + " Mazapanes");
+                    //Pase
+                } else {
+                    JOptionPane.showMessageDialog(null, " Error, precio no permitido");
+
+                }
 
                 break;
 
             default:
-
+                JOptionPane.showMessageDialog(null, " No existe este producto");
         }
 
     }
