51a99e0302fbfab31489b54ad0024fcdf70b9104
PabloNavarro <pnavgarb@g.educaand.es>
2022-10-24 13:34:12 +0200

    Coreccion de ejercicios repeticicion

--- src/main/java/ejerciciosRepeticion/Ej03.java
+++ src/main/java/ejerciciosRepeticion/Ej03.java
@@ -15,23 +15,21 @@ public class Ej03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         //declaracion variable
-       //Realizar un programa que imprima 25 términos de la serie 
-       //15 - 30 - 60 - 120, etc. No se introducen valores por teclado.
-      final int NUMERO_DE_REPETICIONES = 25;
-        int contador=15;
-        int iterador =0;
-       while(iterador<=NUMERO_DE_REPETICIONES){
-           contador*=2; 
-           System.out.println(contador);
-           iterador++; //actualizacion
-            
+        //Realizar un programa que imprima 25 términos de la serie 
+        //15 - 30 - 60 - 120, etc. No se introducen valores por teclado.
+        final int NUMERO_DE_REPETICIONES = 25;
+        int numero = 15;
+        int contador = 0;
+
+        while (contador <= NUMERO_DE_REPETICIONES) {
+            numero *= 2;
+            System.out.println(numero);
+            contador++; //actualizacion
+
         }
-       
-      
-        
-        
+
     }
-    
+
 }
